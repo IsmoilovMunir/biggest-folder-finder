0140bf6ad24e9f6c767bf22ef3e27a9e1b15e464
MunirIsmoilov <ismoilov.munir@gmail.com>
2022-08-08 22:34:03 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
 import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
+//        System.out.println(getHumanReadableSize(240640));
+//        System.out.println(0);
 
 
         String folderPath = "/Users/munirismoilov/Desktop";
         File file = new File(folderPath);
+        Node root = new Node(file);
         long start = System.currentTimeMillis();
-        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        FolderSizeCalculator calculator = new FolderSizeCalculator(root);
         ForkJoinPool pool = new ForkJoinPool();
-        long size = pool.invoke(calculator);
-        System.out.println(size);
-        long duration = System.currentTimeMillis()-start;
+        pool.invoke(calculator);
+        System.out.println(root);
+        long duration = System.currentTimeMillis() - start;
         System.out.println(duration + " ms");
 
     }
 
-    public static long getFolderSize(File folder){
-        if (folder.isFile()){
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
             return folder.length();
         }
         long sum = 0;
         File[] files = folder.listFiles();
-        for (File file:files){
-            sum+=getFolderSize(file);
+        for (File file : files) {
+            sum += getFolderSize(file);
         }
         return sum;
     }
